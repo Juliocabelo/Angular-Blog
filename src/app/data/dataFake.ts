4a831a0741eade1a4573bdd371d5719306ede373
Júlio César <96176317+Juliocabelo@users.noreply.github.com>
2023-07-24 16:53:22 -0300

    Add files via upload

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export
+ const dataFake = [
+  {
+    "id": "1",
+    "title": "Título pra testar ",
+    "description": "Testando a descrição",
+    "photoCover": "https://res.cloudinary.com/cloudinary-marketing/images/v1649718759/Web_Assets/blog/angular_sdk/angular_sdk-png?_i=AA"
+  },
+  {
+    "id": "2",
+    "title": "Outro teste para título",
+    "description": "outro testes para a descrição",
+    "photoCover": "https://kinsta.com/wp-content/uploads/2021/06/php-vs-angular.png"
+  }
+ ]
